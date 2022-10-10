c4bf1b04b24e580b27c9fc9b13511f409b4a5e4e
SrPlissken <srsnkplissken@gmail.com>
2022-10-10 19:17:53 +0200

    Initial commit

---
+++ Conecta4/Conecta4App.swift
@@ -0,0 +1,17 @@
+//
+//  Conecta4App.swift
+//  Conecta4
+//
+//  Created by Victor Melcon Diez on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Conecta4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
